4ff0b00dbd2a0741410172514a4bc3e39fa63dfb
Hosain <cryptichasan925@gmail.com>
2025-04-16 01:56:44 +0600

    set password empty

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,6 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IUser, UserModel } from "./user.interface";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import config from "../../config";
 const userSchema = new Schema<IUser>(
   {
@@ -13,14 +13,14 @@ const userSchema = new Schema<IUser>(
       required: true,
     },
     password: {
-        type: String,
-        required: true,
-        select: false,  
-      },
+      type: String,
+      required: true,
+      select: false,
+    },
     role: {
       type: String,
       enum: ["admin", "user"],
-      default: "user", 
+      default: "user",
     },
     userName: {
       type: String,
@@ -42,27 +42,35 @@ const userSchema = new Schema<IUser>(
 );
 
 // Hash password before saving the user
-userSchema.pre('save', async function (next) {
-    const user = this;
-  
-    // Only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) {
-      return next();
-    }
-  
-    try {
-      user.password = await bcrypt.hash(user.password, Number(config.bcrypt_salt_rounds));
-      next();
-    } catch (error : any) {
-      return next(error);  
-    }
-  });
-  
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    user.password = await bcrypt.hash(
+      user.password,
+      Number(config.bcrypt_salt_rounds)
+    );
+    next();
+  } catch (error: any) {
+    return next(error);
+  }
+});
+
 // Method to find user by email
 userSchema.statics.isUserExistsByEmail = async function (email: string) {
-    return await this.findOne({ email }).select('+password');  
-  };
+  return await this.findOne({ email }).select("+password");
+};
 
+// Set password to an empty string after saving for security
+userSchema.post("save", function (doc, next) {
+  doc.password = ""; // Clear password after saving
+  next();
+});
 
 // Export the user model
-export const User = model<IUser, UserModel>('User', userSchema);
+export const User = model<IUser, UserModel>("User", userSchema);
